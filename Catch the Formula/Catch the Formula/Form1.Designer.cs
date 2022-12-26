e0f349ae427f24e4e840977c46e76aa7bab7cdbb
Samyueru07 <121324812+Samyueru07@users.noreply.github.com>
2022-12-26 21:50:22 +0800

    Add files via upload

---
+++ Catch the Formula/Catch the Formula/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Catch_the_Formula
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtMiss = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 106);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 114);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "Formula";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(328, 106);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 114);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "Formula";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(609, 106);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 114);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "Formula";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::Catch_the_Formula.Properties.Resources.Plankton1;
+            this.player.Location = new System.Drawing.Point(328, 654);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(134, 163);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(12, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(134, 32);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Saved: 0";
+            // 
+            // txtMiss
+            // 
+            this.txtMiss.AutoSize = true;
+            this.txtMiss.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiss.Location = new System.Drawing.Point(604, 9);
+            this.txtMiss.Name = "txtMiss";
+            this.txtMiss.Size = new System.Drawing.Size(119, 32);
+            this.txtMiss.TabIndex = 1;
+            this.txtMiss.Text = "Missed:";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainGameTimerEvents);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Catch_the_Formula.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(761, 829);
+            this.Controls.Add(this.txtMiss);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Catch the Formula";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyisDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyisUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtMiss;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ Catch the Formula/Catch the Formula/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Catch_the_Formula
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft, goRight;
+
+        int speed = 8;
+        int score = 0;
+        int missed = 0;
+
+        Random randX = new Random();
+        Random randY = new Random();
+
+        PictureBox splash = new PictureBox();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void MainGameTimerEvents(object sender, EventArgs e)
+        {
+            txtScore.Text = "Saved: " + score;
+            txtMiss.Text = "Missed: " + missed;
+
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= 12;
+                player.Image = Properties.Resources.Plankton_normal2;
+            }
+            if (goRight == true && player.Left + player.Width < this.ClientSize.Width)
+            {
+                player.Left += 12;
+                player.Image = Properties.Resources.Plankton1;
+            }
+
+            foreach(Control x in this.Controls)
+            {
+
+                if (x is PictureBox && (string)x.Tag == "Formula")
+                {
+
+                    x.Top += speed;
+
+
+                    if(x.Top + x.Height > this.ClientSize.Height)
+                    {
+
+                        splash.Image = Properties.Resources.splash;
+                        splash.Location = x.Location;
+                        splash.Height = 60;
+                        splash.Width = 60;
+                        splash.BackColor = Color.Transparent;
+
+                        this.Controls.Add(splash);
+
+
+                        x.Top = randY.Next(80, 300) * -1;
+                        x.Left = randX.Next(5, this.ClientSize.Width - x.Width);
+                        missed += 1;
+                        player.Image = Properties.Resources.Plankton_hurt;
+
+                    }
+
+
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        x.Top = randY.Next(80, 300) * -1;
+                        x.Left = randX.Next(5, this.ClientSize.Width - x.Width);
+                        score += 1;
+                    }
+
+                }
+            }
+
+            if(score > 10)
+            {
+                speed = 12; 
+            }
+
+            if(missed > 5)
+            {
+                GameTimer.Stop();
+                MessageBox.Show("Game Over!", Environment.NewLine + "We've Lost The secret Formula!" + Environment.NewLine + "Click OK to Retry!");
+                RestartGame();
+            }
+
+        }
+
+        private void KeyisDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+
+        }
+
+        private void KeyisUp(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+
+
+        }
+
+        private void RestartGame()
+        {
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "Formula")
+                {
+                    x.Top = randY.Next(80, 300) * -1;
+                    x.Left = randX.Next(5, this.ClientSize.Width - x.Width);
+                }
+            }
+
+            player.Left = this.ClientSize.Width / 2;
+            player.Image = Properties.Resources.Plankton1;
+
+            score = 0;
+            missed = 0;
+            speed = 8;
+
+            goLeft = false;
+            goRight = false;
+
+            GameTimer.Start();
+
+        }
+
+    }
+}
